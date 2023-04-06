3fbbaa0e9f1a4a7a3a43c33b4ef2fa29f40efb76
ZHAOYUXUAN319 <128475528+ZHAOYUXUAN319@users.noreply.github.com>
2023-04-06 19:09:42 +0900

    Add files via upload

--- sql_answer.sql
+++ sql_answer.sql
@@ -1,5 +1,121 @@
 --問題37！ 49！ 50! 51! 52! 58! 64!
---わからない問題:
+--わからない問題:8,9
+
+
+--問題1：各グループの中でFIFAランクが最も高い国と低い国のランキング番号を表示してください。
+SELECT
+    group_name AS グループ
+    , MIN(ranking) AS ランキング最上位
+    , MAX(ranking) AS ランキング最下位 
+FROM
+    countries 
+GROUP BY
+    group_name
+    
+    
+
+--問題2：全ゴールキーパーの平均身長、平均体重を表示してください
+select avg(height) as 平均身長, avg(weight) as 平均体重 from players
+
+
+
+--問題3：各国の平均身長を高い方から順に表示してください。ただし、FROM句はcountriesテーブルとしてください。
+select
+    countries.name
+    , avg(players.height) 
+from
+    countries join players 
+        on countries.id = players.country_id 
+group by
+    countries.id
+    , countries.name 
+order by
+    avg(players.height) desc
+
+
+
+--問題4：各国の平均身長を高い方から順に表示してください。ただし、FROM句はplayersテーブルとして、テーブル結合を使わず副問合せを用いてください。
+SELECT
+    ( 
+        SELECT
+            c.name 
+        FROM
+            countries c 
+        WHERE
+            p.country_id = c.id
+    ) AS 国名
+    , AVG(p.height) AS 平均身長 
+FROM
+    players p 
+GROUP BY
+    p.country_id 
+ORDER BY
+    AVG(p.height) DESC
+
+
+
+--問題5：キックオフ日時と対戦国の国名をキックオフ日時の早いものから順に表示してください
+SELECT
+    kickoff AS キックオフ日時
+    , c1.name AS 国名1
+    , c2.name AS 国名2 
+FROM
+    pairings p 
+    LEFT JOIN countries c1 
+        ON p.my_country_id = c1.id 
+    LEFT JOIN countries c2 
+        ON p.enemy_country_id = c2.id 
+ORDER BY
+    kickoff
+    
+    
+
+--問題6：すべての選手を対象として選手ごとの得点ランキングを表示してください。（SELECT句で副問合せを使うこと）
+SELECT p.name AS 名前, p.position AS ポジション, p.club AS 所属クラブ, 
+    (SELECT COUNT(id) FROM goals g WHERE g.player_id = p.id) AS ゴール数
+FROM players p
+ORDER BY ゴール数 DESC
+
+
+
+--問題7：すべての選手を対象として選手ごとの得点ランキングを表示してください。（テーブル結合を使うこと）
+SELECT
+    p.name AS 名前
+    , p.position AS ポジション
+    , p.club AS 所属クラブ
+    , COUNT(g.id) AS ゴール数 
+FROM
+    players p 
+    LEFT JOIN goals g 
+        ON g.player_id = p.id 
+GROUP BY
+    p.id
+    , p.name
+    , p.position
+    , p.club 
+ORDER BY
+    ゴール数 DESC
+
+
+
+--問題8：各ポジションごとの総得点を表示してください。
+select p.position,sum(g.id)from players p left join goals g
+on p.id = g.player_id
+group by p.position
+
+
+
+--問題9：ワールドカップ開催当時（2014-06-13）の年齢をプレイヤー毎に表示する。
+SELECT birth,TIMESTAMPDIFF(YEAR, birth, '2014-06-13') AS age, name, position
+FROM players
+ORDER BY age DESC;
+
+
+
+--問題10：オウンゴールの回数を表示する
+SELECT COUNT(g.goal_time)
+FROM goals  g
+WHERE g.player_id IS NULL;
 
 
 
